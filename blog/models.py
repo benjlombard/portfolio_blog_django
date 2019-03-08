b9e9988384d188bc275e3e8891efa2c0f6ca53f3
benjamin <benjamin.lombard.fr@gmail.com>
2019-03-08 21:43:20 +0100

    blog model + admin registration + migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title=models.CharField(max_length=255)
+    body = models.TextField()
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='images/')
